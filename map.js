414eccaf86d91c0e0f7e45bfa969eafcb4de145c
huiyuyie <huiyuyie@gmail.com>
2026-05-14 15:34:24 -0700

    Update

--- map.js
+++ map.js
@@ -1,13 +1,11 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
-import { MAPBOX_TOKEN } from './config.js';
 
-mapboxgl.accessToken = MAPBOX_TOKEN;
+mapboxgl.accessToken = 'pk.pk.eyJ1IjoiaHVpeXV5aWUiLCJhIjoiY21wNXp5M3QyMWoyYjJzcHAwZ2Y4OTlsYyJ9.W1pocc3K7HhQuiUFhDsqug';
 
-// Initialize the map
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v12',
-  center: [-71.09415, 42.36027], // [longitude, latitude]
+  center: [-71.09415, 42.36027],
   zoom: 12,
   minZoom: 5,
   maxZoom: 18
